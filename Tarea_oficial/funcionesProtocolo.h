cc8800587f8dbb9ac7f133a242c3aac9053f3294
JoseM569 <165108931+JoseM569@users.noreply.github.com>
2025-10-25 06:05:39 -0300

    Delete Tarea_oficial directory

--- Tarea_oficial/funcionesProtocolo.h
+++
@@ -1,9 +0,0 @@
-#include "structProtocolo.h"
-
-int empaquetar(protocolo & proto);
-
-int fcs(BYTE * array,int tam);
-
-bool desempaquetar(protocolo & proto);
-
-void enviarFrame(int pin, int speed, protocolo proto, int largo);
